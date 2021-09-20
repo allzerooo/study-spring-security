e6d019add0311e10d5b695929776c3b015a71f3e
ddalam <dychoi.dev@gmail.com>
2021-09-20 17:00:39 +0900

    security filter chain 주석 추가

--- server/basic/src/main/java/com/study/security/config/SecurityConfig.java
+++ server/basic/src/main/java/com/study/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.study.security.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Order(1)   // security filter chain을 여러개 구성할 경우 순서 지정
 @EnableWebSecurity(debug = true)    // security 필터를 어떻게 구성했는지 터미널 로그로 확인할 수 있음
 @EnableGlobalMethodSecurity(prePostEnabled = true)  // pre post로 권한 체크를 하겠다 -> 권한 체크 모듈이 작동됨
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -34,8 +36,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     // 스프링 시큐리티는 기본적으로 모든 페이지를 다 막아둔다
+    // security 필터를 구성하는 설정
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.antMatcher("/api/**"); // 어떤 request에 대해서 필터 체인이 동작할 것인지 설정
         http.authorizeRequests((requests) ->
 //                requests.anyRequest().authenticated() // 모든 페이지를 다 인증해라
                 requests.antMatchers("/").permitAll()   // / 주소는 사용자에게 다 접근을 허락해라
