f1514c23fd1f2609a3293f1c6f9dfc5b8db30b99
ddalam <dychoi.dev@gmail.com>
2021-09-21 10:49:14 +0900

    feat(login-basic): 로그인 방법, 로그인 페이지 설정

--- server/login-basic/src/main/java/com/study/security/config/SecurityConfig.java
+++ server/login-basic/src/main/java/com/study/security/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.study.security.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -17,6 +19,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                             .anyRequest().authenticated()   // anyRequest에 대해서는 허락을 받고 들어오도록
                             ;
                 })
+                // usernamepassword를 formLogin으로 설정. login 페이지를 정해주지 않으면 DefaultLoginPageGeneratingFilter, DefaultLogoutPageGeneratingFilter 가 동작을 하게 된다
+                .formLogin(
+                        // permitAll을 하지 않으면 anyRequest().authenticated() 에 의해 로그인 페이지로 이동하고, 로그인 페이지는 접근할 수 없기 때문에 무한루프에 빠질 수 있다
+                        login -> login.loginPage("/login").permitAll()
+                )
                 ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // request에 인증을 걸었을 때 css와 같은 리소스도 접근되지 않기 때문에 제외시켜줌
+        web.ignoring()
+                .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations()
+                );
     }
 }
