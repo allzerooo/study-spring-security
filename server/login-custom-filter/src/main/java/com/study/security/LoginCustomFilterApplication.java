eb9913df3426c1c24b7a343f0170e8126437c5e4
ddalam <dychoi.dev@gmail.com>
2021-09-23 22:08:10 +0900

    login-custom-filter 모듈 추가

---
+++ server/login-custom-filter/src/main/java/com/study/security/LoginCustomFilterApplication.java
@@ -0,0 +1,14 @@
+package com.study.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginCustomFilterApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginCustomFilterApplication.class, args);
+    }
+
+}
