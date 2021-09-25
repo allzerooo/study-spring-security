bd15a7fa131f3a12e8a8251d946ecc39d706fbaa
ddalam <dychoi.dev@gmail.com>
2021-09-25 17:35:47 +0900

    Student를 principal로 하는 로그인

--- server/login-custom-filter/src/main/java/com/study/security/config/SecurityConfig.java
+++ server/login-custom-filter/src/main/java/com/study/security/config/SecurityConfig.java
@@ -1,21 +1,39 @@
 package com.study.security.config;
 
+import com.study.security.student.StudentManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@RequiredArgsConstructor
 @EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final StudentManager studentManager;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(studentManager);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(request->
-                        request.anyRequest().permitAll()
-                )
+        .authorizeRequests(request->
+                request.antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+        )
+        .formLogin(
+                // login page를 통해서 usernamePasswordAuthenticationFilter가 동작하고
+                // usernamePasswordAuthenticationFilter는 usernamePasswordAuthenticationToken을 발행하기 때문에
+                // usernamePasswordAuthenticationToken을 대상으로 한 StudentManager가 발행을 해주게 된다
+                login -> login.loginPage("/login")
+                .permitAll()
+        )
                 ;
     }
 
--- server/login-custom-filter/src/main/java/com/study/security/student/StudentManager.java
+++ server/login-custom-filter/src/main/java/com/study/security/student/StudentManager.java
@@ -1,14 +1,15 @@
 package com.study.security.student;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.Set;
 
 @Component
 // 통행증을 발급해줄 provider
@@ -20,7 +21,15 @@ public class StudentManager implements AuthenticationProvider, InitializingBean 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
-
+        if (studentDB.containsKey(token.getName())) {
+            Student student = studentDB.get(token.getName());
+            // studentAuthenticationToken을 만들어서 return
+            return StudentAuthenticationToken.builder()
+                    .principal(student)
+                    .details(student.getUsername())
+                    .authenticated(true)
+                    .build();
+        }
         return null;
     }
 
@@ -34,6 +43,10 @@ public class StudentManager implements AuthenticationProvider, InitializingBean 
     @Override
     public void afterPropertiesSet() throws Exception {
         // InitializingBean에 의해 빈이 초기화되었을 때 실핼되는 코드
-
+        Set.of(
+                new Student("hong", "홍길동", Set.of(new SimpleGrantedAuthority("ROLE_USER"))),
+                new Student("kang", "강아지", Set.of(new SimpleGrantedAuthority("ROLE_USER"))),
+                new Student("rang", "호랑이", Set.of(new SimpleGrantedAuthority("ROLE_USER")))
+        ).forEach(s -> studentDB.put(s.getId(), s));
     }
 }
